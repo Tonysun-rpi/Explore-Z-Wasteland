c6afc4519c8170db32378696ff0d48fc54627f77
Misaosama <42242012+Misaosama@users.noreply.github.com>
2018-10-18 18:09:33 -0400

    猛汉

--- monste&&item/monster/monster.php
+++ monste&&item/monster/monster.php
@@ -11,25 +11,69 @@ class monster
 {
     //var $redis;
     var $name;
+    var $damage;
     var $health;
     var $magic_resistance;
     var $armor;
+    var $max_health;
+    var $redis;
 
     var $wealth; //掉落金币
-    var $reword; //掉落素材
+    var $reward_1; //掉落素材
+    var $reward_2;
+    var $reward_3;
+    var $reward_4;
+    var $reward_rate; //掉落素材倍率
 
     function __construct( $n ){
-        $redis=connectDB();
+        $this->redis=connectDB();
         $this->name=$n;
+        $this->damage=$redis->hget($n,"damage");
         $this->health=$redis->hget($n,"health");
         $this->magic_resistance=$redis->hget($n,"magic_resistance");
         $this->armor=$redis->hget($n,"armor");
         $this->wealth=$redis->hget($n,"wealth");
+        $this->reward_1=$redis->hget($n,"reward_1");
+        $this->reward_2=$redis->hget($n,"reward_2");
+        $this->reward_3=$redis->hget($n,"reward_3");
+        $this->reward_4=$redis->hget($n,"reward_4");
+        $this->reward_rate=$redis->hget($n,"reward_rate");
+        $this->max_health=$this->health;
         //here
 
 
 
     }
+
+    public function get_info(){
+        $info=[$this->health,$this->damage,$this->armor,$this->magic_resistance];
+        return $info;
+    }
+
+    public function give_reword(){
+        //p 是获得该物品的概率
+        $p1=$this->redis->hget("reward",$this->reward_1)*$this->reward_rate;
+        $p2=$this->redis->hget("reward",$this->reward_2)*$this->reward_rate;
+        $p3=$this->redis->hget("reward",$this->reward_3)*$this->reward_rate;
+        $p4=$this->redis->hget("reward",$this->reward_4)*$this->reward_rate;
+        $reward_list=array($this->reward_1=>$p1,$this->reward_2=>$p2,
+                            $this->reward_3=>$p3,$this->reward_4=>$p4);
+        foreach($reward_list as $key=>$value){
+            $random=rand(0,1);
+            if($random>$value){
+                $reward_list[$key]=0; //如果random比概率大，不获得该物品
+            }
+            else{
+                $num=ceil($value/$random);
+                $reward_list[$key]=$num;
+            }
+        }
+        return $reward_list;
+    }
+
+
+
+
 
     function is_attacted($h){
         $this->health-=$h;
---
+++ monste&&item/weapon/weapon.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangj
+ * Date: 2018/10/18
+ * Time: 17:28
+ */
+
+class weapon
+{
+    var $damage;
+    var $id;
+
+
+}
+
+?>
