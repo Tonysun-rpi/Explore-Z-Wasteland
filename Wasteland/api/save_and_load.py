af1290aae9d9e3ba10ad0d71b77ec07b46515d0f
ruizhe <lir7@rpi.edu>
2018-12-09 02:19:04 -0500

    brief description about stored data
    explain what's the "save_file"

--- Wasteland/api/save_and_load.py
+++ Wasteland/api/save_and_load.py
@@ -1,9 +1,28 @@
 # TODO: This S/L api uses txt files for the time being, should use databases in the future
 
+# questions: what's in the save_file?
+# save_file is a dictionary contains the following keys:
+# profile, bag, base, and storage
+# They are all "class", but bag and storage contains a list(need to keep the position of items)
+
+# what's in the profile?
+# name, level, gender, age, hunger, health, weapon, armor
+
+# what's in the base?
+# name, level, storage
+
+# what's in the bag?
+# name, volume, items(list)
+
+# what's in the storage?
+# name, volume, items(list)
+
 
 # save_file: the object that contains all the information
 # filename: filename for save file
 def save(save_file, filename):
+    #f = open(save_file, "w") 
+    #f.write(save_file)
 	pass
 
 
